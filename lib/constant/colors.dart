4f1e6522012baba3668df515730e6a9a9680eb5b
KinzaZahid <142378115+KinziShiekh@users.noreply.github.com>
2025-02-26 10:01:29 -0800

    Delete lib directory

--- lib/constant/colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-// Define a class to hold custom colors
-class AppColors {
-  static const MainColor = Color(0xFFf16611);
-  static const Background =
-      Color.fromARGB(255, 255, 255, 255); // Example orange color
-}
